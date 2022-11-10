79aa21934cd52098e74453db5c4313264e3fd5ac
Abraham Segura <abrahamdsm299@gmail.com>
2022-11-10 12:26:03 -0400

    create theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
-
+      <h1>StartPoint</h1>
     </div>
   )
 }
---
+++ src/context/theme.js
@@ -0,0 +1,15 @@
+import { ThemeContext } from "@emotion/react"
+import styled from "@emotion/styled"
+
+export function theme() {
+    const theme = {
+        primary: {
+            light: {
+                backgroundColor: '#f3f3f3'
+            },
+            dark: {
+                backgroundColor: '#000'
+            }
+        }
+    }
+}
